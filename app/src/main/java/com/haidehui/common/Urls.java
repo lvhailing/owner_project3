81386cff7485d0e2425db3e5d519c82f73434df7
lvhl3 <lvhl3@ziroom.com>
2017-11-09 18:06:01 +0800

    正式环境地址变更测试

--- app/src/main/java/com/haidehui/common/Urls.java
+++ app/src/main/java/com/haidehui/common/Urls.java
@@ -9,7 +9,8 @@ public class Urls {
     public static String URL_OUTER_NET_DEBUG = "http://123.126.102.219:21083/";
 
     // 正式环境IP
-    public static String URL_OFFICIAL = "https://hwapp.cf360.com/";
+//    public static String URL_OFFICIAL = "https://hwapp.cf360.com/";
+    public static String URL_OFFICIAL = "https://app.jdjufu.com/";  //( 再发版时用这个新的正式地址)
 
     //代树理
     public static final String URL_DSL = "http://192.168.1.106:9999/overseas-app/";
@@ -27,7 +28,7 @@ public class Urls {
     public static final String URL_FYM = "http://192.168.1.164:9998/overseas-app/";
 
     // 调试，上线时只需改此处环境即可
-    private static String EC_HOST = URL_OUTER_NET_DEBUG;
+    private static String EC_HOST = URL_OFFICIAL;
 
     /**
      * 登出
